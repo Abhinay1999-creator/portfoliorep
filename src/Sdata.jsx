723ede2246383e95a0d9d3f4657faa6753a4e92b
Abhinay1999-creator <85627323+Abhinay1999-creator@users.noreply.github.com>
2021-08-01 22:01:14 +0530

    image

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -2,7 +2,7 @@ import app from "./Images/netflix-logo.jpg";
 import web from "./Images/COVID19.jpg";
 import git from "./Images/github.jpg";
 import music from "./Images/music.jpg";
-import logo from "./Images/logo.png";
+import logo from "./Images/th.jpg";
 
 const Sdata = [
     {
